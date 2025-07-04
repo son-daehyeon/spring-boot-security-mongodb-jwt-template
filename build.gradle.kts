5fc0a16a77e4e930ea1b9bbe9f7400909800314a
Son Daehyeon <sondaehyeon01@gmail.com>
2025-07-04 10:42:09 +0900

    chore(deps): Spring Boot 버전 업그레이드

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.4.3"
-    id("io.spring.dependency-management") version "1.1.6"
+    id("org.springframework.boot") version "3.5.3"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.github.son-daehyeon"
@@ -33,5 +33,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    implementation("com.auth0:java-jwt:4.4.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.4")
+
+    implementation("com.auth0:java-jwt:4.5.0")
 }
